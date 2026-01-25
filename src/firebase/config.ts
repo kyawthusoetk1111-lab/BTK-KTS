8cff88b7123473dbfe403e98c847bf38292870c8
Kyaw Thu Soe <kyawthusoetk1111@gmail.com>
2026-01-25 12:51:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5216046318-e9f06",
+  "appId": "1:1064933833999:web:3ce4a36c083fbba870127a",
+  "apiKey": "AIzaSyBN0R8t3t9mteTJUmoTEuphL1QbgW__doo",
+  "authDomain": "studio-5216046318-e9f06.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1064933833999"
+};
